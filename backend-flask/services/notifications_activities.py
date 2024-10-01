0ee7166bd7c4e454bc57dc0481082a511d28a31b
Ray Kim <kimbm2014@gmail.com>
2024-10-01 23:30:18 +0000

    add segments and subsegments

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,27 +1,43 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
+
 class NotificationsActivities:
   def run():
-    now = datetime.now(timezone.utc).astimezone()
-    results = [{
-      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'coco',
-      'message': 'I am a while unicorn',
-      'created_at': (now - timedelta(days=2)).isoformat(),
-      'expires_at': (now + timedelta(days=5)).isoformat(),
-      'likes_count': 5,
-      'replies_count': 1,
-      'reposts_count': 0,
-      'replies': [{
-        'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
-        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Worf',
-        'message': 'This post has no honor!',
-        'likes_count': 0,
-        'replies_count': 0,
+      # X-Ray
+      segment = xray_recorder.begin_segment('notifications_activities') 
+      now = datetime.now(timezone.utc).astimezone()
+
+     
+     
+
+      results = [{
+        'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle':  'coco',
+        'message': 'I am a while unicorn',
+        'created_at': (now - timedelta(days=2)).isoformat(),
+        'expires_at': (now + timedelta(days=5)).isoformat(),
+        'likes_count': 5,
+        'replies_count': 1,
         'reposts_count': 0,
-        'created_at': (now - timedelta(days=2)).isoformat()
-      }],
-     },
-  
-    ]
+        'replies': [{
+          'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
+          'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+          'handle':  'Worf',
+          'message': 'This post has no honor!',
+          'likes_count': 0,
+          'replies_count': 0,
+          'reposts_count': 0,
+          'created_at': (now - timedelta(days=2)).isoformat()
+        }],
+      },
+    
+      ]
+
+      subsegment = xray_recorder.begin_subsegment('mock-data')
+        # X-Ray
+      dict = {
+        "now": now.isoformat(),
+        "result-size": len(model['data'])
+      }
+      subsegment.put_metadata('key', dict, 'namespace')
     return results
